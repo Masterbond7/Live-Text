4850b4c7eb731ee2a483eac1775936ba35b56336
Masterbond7 <jamesslough4@gmail.com>
2022-04-06 10:09:53 +1200

    Added code to initialize the inotify instance and return an error in the terminal if unable to, infinate loop to cause an error for testing purpouses also added

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-// Including libraries
+// Including general files
 #include <iostream>
+#include <errno.h>
+#include <string.h>
+
+// Including inotify files
+#include <sys/inotify.h>
 
 // Setting namespace
 using namespace std;
 
+// Setting template strings
+const char TEMPLATE_ERROR[] = "\033[31m[ERROR]\033[0m";
+
 // Main function
 int main() {
-	// Printing hello world
-	cout << "Hello World!" << endl;
+	while (true){
+	// Start an inotify instance
+	int fd = inotify_init();
+	if (fd == -1) {
+		printf("%s Inotify initialization error\nErrno %i\n", TEMPLATE_ERROR, errno);
+		return errno;
+	}}
 
 	// Exiting the program successfully
 	return 0;
