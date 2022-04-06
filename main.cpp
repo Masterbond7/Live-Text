786ac47f905835a396b13e359bae1dde54f331d6
Masterbond7 <jamesslough4@gmail.com>
2022-04-06 12:39:08 +1200

    Added code to add a watch to the inotify instance

--- main.cpp
+++ main.cpp
@@ -15,9 +15,16 @@ const char TEMPLATE_ERROR[] = "\033[31m[ERROR]\033[0m";
 // Main function
 int main() {
 	// Start an inotify instance
-	int fd = inotify_init();
+	int fd = inotify_init(); // File descriptor
 	if (fd == -1) {
 		printf("%s Inotify initialization error\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
+		return errno;
+	}
+
+	// Add an inotify watch
+	int wd = inotify_add_watch(fd, "/home/james/Desktop/", IN_ACCESS | IN_CLOSE_WRITE | IN_CLOSE_NOWRITE | IN_CREATE | IN_DELETE | IN_MODIFY | IN_OPEN); // Watch descriptor
+	if (wd == -1) {
+		printf("%s Inotify watch start error\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
 		return errno;
 	}
 
