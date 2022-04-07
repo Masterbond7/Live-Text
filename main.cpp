9c2a973cb733b3c6eff3332bd43eacafbdce8742
Masterbond7 <jamesslough4@gmail.com>
2022-04-08 07:38:17 +1200

    Added print and if statements to show what exactly each operation does.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ const char TEMPLATE_ERROR[] = "\033[31m[ERROR]\033[0m";
 // Main function
 int main() {
 	// Start an inotify instance
+	printf("Starting inotify instance\n");
 	int fd = inotify_init(); // File descriptor
 	if (fd == -1) {
 		printf("%s Inotify initialization error\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
@@ -23,7 +24,8 @@ int main() {
 	}
 
 	// Add an inotify watch
-	int wd = inotify_add_watch(fd, "/home/james/Desktop/", IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO); // Watch descriptor 
+	printf("Adding inotify watch\n");
+	int wd = inotify_add_watch(fd, "/home/james/Desktop/", IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO); // Watch descriptor
     //(may need to add IN_CLOSE_WRITE)
 	if (wd == -1) {
 		printf("%s Inotify watch start error\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
@@ -31,19 +33,26 @@ int main() {
 	}
 
 	// Read events from file descriptor
-	char buf[4096];
-	ssize_t len = read(fd, buf, sizeof(buf));
-	const struct inotify_event *event;
-	if (len == -1) {
-		printf("%s Inotify read\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
-	} else {
-		printf("File event detected\n");
-	}
+	printf("Reading events\n");
+	while (true) {
+		char buf[4096]; // Optimize so it's always the right size (inotify documentation: notes)
+		ssize_t len = read(fd, buf, sizeof(buf));
+		if (len == -1) {
+			printf("%s Inotify read\nErrno %i, (%s)\n", TEMPLATE_ERROR, errno, strerror(errno));
+			return errno;
+		}
 
-	// Interpret events from buffer
-	for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
-		event = (const struct inotify_event *) ptr;
-		cout << event->mask << endl;
+		// Interpret events from buffer
+		const struct inotify_event *event;
+		for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
+			event = (const struct inotify_event *) ptr;
+			if (event->mask & IN_CREATE) { printf("IN_CREATE: %s\n", event->name); }
+			else if (event->mask & IN_DELETE) { printf("IN_DELETE: %s\n", event->name); }
+			else if (event->mask & IN_MODIFY) { printf("IN_MODIFY: %s\n", event->name); }
+			else if (event->mask & IN_MOVED_FROM) { printf("IN_MOVED_FROM: %s, %i\n", event->name, event->cookie); }
+			else if (event->mask & IN_MOVED_TO) { printf("IN_MOVED_TO: %s %i\n", event->name, event->cookie); }
+			//cout << event->mask << endl;
+		}
 	}
 
 	// Exiting the program successfully
